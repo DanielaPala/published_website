ebdcb52f09b093d7c9598144edbeabd813fded8a
MalpenZibo <MalpenZibo@users.noreply.github.com>
2023-08-23 17:21:58 +0000

    deploy: DanielaPala/website@633f2319f032f97ea41078485d17b2981258864f

--- 662fefa955a9852b1942867c8f9158fdf2f5292d/script.js
+++ 662fefa955a9852b1942867c8f9158fdf2f5292d/script.js
@@ -10,7 +10,6 @@ menuVoices.forEach((i) => {
 window.addEventListener("scroll", function () {
   var header = document.querySelector(".header");
   var scrollPosition = window.scrollY;
-
   if (scrollPosition > 0) {
     header.classList.add("small");
   } else {
