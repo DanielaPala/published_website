4ada73a9ee92bb9fcb15e877cc9bb3cbcaec36e2
MalpenZibo <MalpenZibo@users.noreply.github.com>
2023-08-24 16:37:33 +0000

    deploy: DanielaPala/website@2d4b79172034d3d90a8ed0f1db137aab46f593ba

--- 662fefa955a9852b1942867c8f9158fdf2f5292d/script.js
+++ 662fefa955a9852b1942867c8f9158fdf2f5292d/script.js
@@ -1,10 +1,16 @@
 const checkbox = document.querySelector(".menu-toggle input");
 const menuVoices = document.querySelectorAll(".header-menu li a");
+const dimmer = document.querySelector(".dimmer");
 
 menuVoices.forEach((i) => {
   i.addEventListener("click", () => {
     checkbox.checked = false;
   });
+});
+
+dimmer.addEventListener("click", () => {
+  console.log("ciao");
+  checkbox.checked = false;
 });
 
 window.addEventListener("scroll", function () {
