dc2554e66639884e66e59616a3200fef51e35021
MalpenZibo <MalpenZibo@users.noreply.github.com>
2023-08-31 15:23:57 +0000

    deploy: DanielaPala/website@36b9b1b08ff679d6209e02ede2b32f717f60583d

--- 662fefa955a9852b1942867c8f9158fdf2f5292d/script.js
+++ 662fefa955a9852b1942867c8f9158fdf2f5292d/script.js
@@ -13,12 +13,3 @@ dimmer.addEventListener("click", () => {
   checkbox.checked = false;
 });
 
-window.addEventListener("scroll", function () {
-  var header = document.querySelector(".header");
-  var scrollPosition = window.scrollY;
-  if (scrollPosition > 0) {
-    header.classList.add("small");
-  } else {
-    header.classList.remove("small");
-  }
-});
